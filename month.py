dcfef5410dc0860c7b31f3ad07b0f5c3ed5c70e7
Ananya-4JN22IS025 <157824878+Ananya-4JN22IS025@users.noreply.github.com>
2025-08-22 15:03:21 +0530

    Add files via upload

---
+++ month.py
@@ -0,0 +1,8 @@
+month=input("enter the name of month")
+days=31
+if month=="April"or month=="june"or month=="September"or month=="november:
+days=30
+elif month="february "
+    days="28 or 29"
+    print(month, "has", days, " days in it") 
+  
